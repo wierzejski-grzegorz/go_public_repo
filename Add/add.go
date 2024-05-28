7eb95174a36f47eb005d3a9df4fd753a8af279b9
Grzegorz Wierzejski <grzegorz.w@mindojo.com>
2024-05-28 12:03:05 +0200

    Added doc

--- Add/add.go
+++ Add/add.go
@@ -1,5 +1,11 @@
+// Package go_public_repo is a public package created for the purpose of completing the exercise from the book.
+// This package contains a function that takes two integers and returns their sum.
 package go_public_repo
 
+// Add takes two integers and returns their sum.
+// More information about the function can be found at [AdditionExplanation].
+//
+// [AdditionExplanation]: https://www.mathsisfun.com/numbers/addition.html
 func Add(x, y int) int {
 	return x + y
 }
