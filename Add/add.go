2befdaa46a22628d6ddf71bc717839b7678b7f4a
Grzegorz Wierzejski <grzegorz.w@mindojo.com>
2024-05-28 12:08:10 +0200

    Made Add generic

--- Add/add.go
+++ Add/add.go
@@ -2,10 +2,16 @@
 // This package contains a function that takes two integers and returns their sum.
 package go_public_repo
 
-// Add takes two integers and returns their sum.
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add takes two numbers and returns their sum.
 // More information about the function can be found at [AdditionExplanation].
 //
 // [AdditionExplanation]: https://www.mathsisfun.com/numbers/addition.html
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wierzejski-grzegorz/go_public_repo
 
 go 1.22.2
+
+require golang.org/x/exp v0.0.0-20240525044651-4c93da0ed11d
